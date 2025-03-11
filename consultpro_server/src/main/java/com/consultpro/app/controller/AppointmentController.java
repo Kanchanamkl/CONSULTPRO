413705576faf202caa9f68acf418ca3cdc892ed0
Kanchana Kalansooriya <92777840+Kanchanamkl@users.noreply.github.com>
2025-03-11 19:27:58 +0530

    corss orgin allow issue fix change

--- consultpro_server/src/main/java/com/consultpro/app/controller/AppointmentController.java
+++ consultpro_server/src/main/java/com/consultpro/app/controller/AppointmentController.java
@@ -17,6 +17,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/appointments")
 @RequiredArgsConstructor
 public class AppointmentController {
