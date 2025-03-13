e6c609ea2358cfd71c4165f7b09262f35dd32c37
Kanchana Kalansooriya <92777840+Kanchanamkl@users.noreply.github.com>
2025-03-13 22:45:42 +0530

    frontend port change 80 to 3000

--- consultpro_server/src/main/java/com/consultpro/app/config/ApplicationConfig.java
+++ consultpro_server/src/main/java/com/consultpro/app/config/ApplicationConfig.java
@@ -64,7 +64,7 @@ public class ApplicationConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
 //        config.setAllowedOrigins(List.of("http://142.93.215.196:5173"));
-        config.setAllowedOrigins(List.of("http://142.93.215.196","http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://142.93.215.196:3000","http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Origin", "Content-Type", "Accept", "Authorization"));
         config.setAllowCredentials(true);
